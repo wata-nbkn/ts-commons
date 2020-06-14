08feb6e01e547cb36cc0e0e24e49b5cf69d121d4
NBKN <knt.watanab@gmail.com>
2020-06-14 19:32:38 +0900

    fix date utils

---
+++ src/dateUtils/index.ts
@@ -0,0 +1,5 @@
+export { getMoment } from './getMoment';
+export { format } from './format';
+export { isValid } from './isValid';
+export { getToday, getCurrentMonth, getCurrentYear, getMonth, getYear } from './getDates';
+export { isAfterDate, isBeforeDate, isSameDate, isSameOrAfterDate, isSameOrBeforeDate } from './compareDates';
