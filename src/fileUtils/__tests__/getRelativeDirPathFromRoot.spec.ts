3322e28acccc8c5402d330c438c70a11848f0eef
NBKN <knt.watanab@gmail.com>
2020-06-14 19:30:42 +0900

    fix tests

--- src/fileUtils/__tests__/getRelativeDirPathFromRoot.spec.ts
+++ src/fileUtils/__tests__/getRelativeDirPathFromRoot.spec.ts
@@ -1,7 +1,7 @@
 import { getRelativeDirPathFromRoot } from '../getRelativeDirPathFromRoot';
 
 describe('getRelativeDirPathFromRoot', () => {
-  it('should make dir path from the root dir', () => {
+  it('should return the dir path from the root dir', () => {
     const path = getRelativeDirPathFromRoot(__filename);
     expect(path).toEqual('./fileUtils/__tests__');
   });
--- src/mathUtils/__tests__/getRand.spec.ts
+++ src/mathUtils/__tests__/getRand.spec.ts
@@ -1,13 +1,13 @@
 import { getRand } from '../getRand';
 
 describe('getRand', () => {
-  it('should make a random value', () => {
+  it('should return a random value', () => {
     const result = getRand();
     expect(result).toBeGreaterThanOrEqual(1);
     expect(result).toBeLessThanOrEqual(10);
   });
 
-  it('should make a random value with a range', () => {
+  it('should return a random value with a range', () => {
     const result = getRand(-1, -100);
     expect(result).toBeGreaterThanOrEqual(-100);
     expect(result).toBeLessThanOrEqual(-1);
