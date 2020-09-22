21d1a09828373c14e2d317d0018a26808345df5a
NBKN <knt.watanab@gmail.com>
2020-09-22 21:59:37 +0900

    make div options for calc methods

--- src/utils/mathUtils/__tests__/calc.spec.ts
+++ src/utils/mathUtils/__tests__/calc.spec.ts
@@ -1,27 +1,54 @@
 import { div, calcPercentage, calcDiffPercentage } from '../calc';
 
 describe('calc', () => {
-  it('div', () => {
-    let result = div(100, 3);
-    expect(result).toEqual(33.33);
+  describe('div', () => {
+    it('should return with 2 digits with truncated', () => {
+      const result = div(2, 3);
+      expect(result).toEqual(0.66);
+    });
 
-    result = div(0, 10);
-    expect(result).toEqual(0);
+    it('should return 0', () => {
+      const result = div(0, 10);
+      expect(result).toEqual(0);
+    });
 
-    result = div(10, 0);
-    expect(result).toEqual('-');
+    it('should return `-` when 0 is provided', () => {
+      const result = div(100, 0);
+      expect(result).toEqual('-');
+    });
 
-    result = div(null, null);
-    expect(result).toEqual('-');
+    it('should return `-` when a null is provided', () => {
+      const result = div(null, 100);
+      expect(result).toEqual('-');
+    });
 
-    result = div(undefined, undefined);
-    expect(result).toEqual('-');
+    it('should return `-` when a undefined is provided', () => {
+      const result = div(100, undefined);
+      expect(result).toEqual('-');
+    });
+
+    it('should return `-` when a string is provided', () => {
+      const result = div(100, 'b');
+      expect(result).toEqual('-');
+    });
+
+    describe('with options', () => {
+      it('should return with 2 digits with fixed', () => {
+        const result = div(2, 3, { isFixed: true });
+        expect(result).toEqual(0.67);
+      });
+
+      it('should return with more digit', () => {
+        const result = div(2, 3, { decimalPlaces: 4, isFixed: true });
+        expect(result).toEqual(0.6667);
+      });
+    });
   });
 
   describe('calcPercentage', () => {
-    it('should return with 2 digits', () => {
-      const result = calcPercentage(1, 3);
-      expect(result).toEqual(33.33);
+    it('should return with 2 digits with truncated', () => {
+      const result = calcPercentage(2, 3);
+      expect(result).toEqual(66.66);
     });
 
     it('should return 0', () => {
@@ -38,12 +65,24 @@ describe('calc', () => {
       const result = calcPercentage(100, 0);
       expect(result).toEqual('-');
     });
+
+    describe('with options', () => {
+      it('should return with 2 digits with fixed', () => {
+        const result = calcPercentage(2, 3, { isFixed: true });
+        expect(result).toEqual(66.67);
+      });
+
+      it('should return with more digit', () => {
+        const result = calcPercentage(2, 3, { decimalPlaces: 4, isFixed: true });
+        expect(result).toEqual(66.6667);
+      });
+    });
   });
 
   describe('calcDiffPercentage', () => {
-    it('should return with 2 digits', () => {
-      const result = calcDiffPercentage(1, 3);
-      expect(result).toEqual(-66.67);
+    it('should return with 2 digits with truncated', () => {
+      const result = calcDiffPercentage(5, 3);
+      expect(result).toEqual(66.66);
     });
 
     it('should return 0', () => {
@@ -59,6 +98,18 @@ describe('calc', () => {
     it('should return `-` when 0 is provided', () => {
       const result = calcDiffPercentage(100, 0);
       expect(result).toEqual('-');
+    });
+
+    describe('with options', () => {
+      it('should return with 2 digits with fixed', () => {
+        const result = calcDiffPercentage(5, 3, { isFixed: true });
+        expect(result).toEqual(66.67);
+      });
+
+      it('should return with more digit', () => {
+        const result = calcDiffPercentage(5, 3, { decimalPlaces: 4, isFixed: true });
+        expect(result).toEqual(66.6667);
+      });
     });
   });
 });
--- src/utils/mathUtils/calc.ts
+++ src/utils/mathUtils/calc.ts
@@ -1,26 +1,51 @@
 import { Value } from 'types';
 import { isNumber } from './isNumber';
 
-export const div = (valA: any, valB: any): Value => {
-  if (!isNumber(valA) || !isNumber(valB) || valB === 0) {
+export type DivOptions = {
+  decimalPlaces?: number;
+  isFixed?: boolean;
+  isTruncated?: boolean;
+};
+
+const DEFAULT_DECIMAL_PLACES = 2;
+const DEFAULT_DIV_OPTIONS = {
+  decimalPlaces: DEFAULT_DECIMAL_PLACES,
+  isFixed: false,
+  isTruncated: true,
+};
+
+const validValues = (valA: any, valB: any) => isNumber(valA) && isNumber(valB) && valB !== 0;
+
+const handleDecimal = (val: number, options?: DivOptions) => {
+  const { decimalPlaces = DEFAULT_DECIMAL_PLACES, isFixed, isTruncated } = options || DEFAULT_DIV_OPTIONS;
+  if (isFixed) {
+    return Number(val.toFixed(decimalPlaces));
+  } else if (isTruncated) {
+    const digit = Math.pow(10, decimalPlaces);
+    return Math.floor(val * digit) / digit;
+  }
+  return val;
+};
+
+export const div = (valA: any, valB: any, options?: DivOptions): Value => {
+  if (!validValues(valA, valB)) {
     return '-';
   }
   const val = valA / valB;
-  return Number(val.toFixed(2));
+  return handleDecimal(val, options);
 };
 
-export const calcPercentage = (valA: any, valB: any) => {
-  if (!isNumber(valA) || !isNumber(valB) || valB === 0) {
+export const calcPercentage = (valA: any, valB: any, options?: DivOptions) => {
+  if (!validValues(valA, valB)) {
     return '-';
   }
   const val = (valA / valB) * 100;
-  return Number(val.toFixed(2));
+  return handleDecimal(val, options);
 };
 
-export const calcDiffPercentage = (valA: any, valB: any) => {
-  if (!isNumber(valA) || !isNumber(valB) || valB === 0) {
+export const calcDiffPercentage = (valA: any, valB: any, options?: DivOptions) => {
+  if (!validValues(valA, valB)) {
     return '-';
   }
-  const val = ((valA - valB) / valB) * 100;
-  return Number(val.toFixed(2));
+  return calcPercentage(valA - valB, valB, options);
 };
